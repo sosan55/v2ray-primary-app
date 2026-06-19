83ac7081ff4c9a0e0865303dabf312758c52a7a3
kayamavy <150877815+maryoa61@users.noreply.github.com>
2026-06-19 07:28:29 +0330

    build: enable legacy native library packaging
    Configure extractNativeLibs and useLegacyPackaging to ensure compatibility with native V2Ray binaries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,11 @@ android {
     buildConfig = true
   }
   testOptions { unitTests { isIncludeAndroidResources = true } }
+  packaging {
+    jniLibs {
+      useLegacyPackaging = true
+    }
+  }
 }
 
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
